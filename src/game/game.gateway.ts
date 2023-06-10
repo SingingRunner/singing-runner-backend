527dcef22a9b2b92bce132f74095c12ab66d1749
Recordum <mingyu.oh15b@gmail.com>
2023-06-11 07:33:22 +0900

    Feat: [#2] game.gateway 핸들러 추가

--- src/game/game.gateway.ts
+++ src/game/game.gateway.ts
@@ -1,9 +1,44 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { ConnectedSocket, OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway()
-export class GameGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+export class EventsGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect{
+  
+  @WebSocketServer() server: Server;
+  
+  constructor(
+    
+  ) {}
+
+  afterInit(server: Server) {
+    console.log('Socket.io server initialized in ');
+  }
+
+  handleConnection(@ConnectedSocket() client: Socket) {
+    console.log(`Client connected: ${client.id}`);
+  }
+
+  handleDisconnect(@ConnectedSocket() client: Socket) {
+    console.log(`Client disconnected: ${client.id}`);
+  }
+
+  @SubscribeMessage('match_connected')
+  matchConnectedData(@ConnectedSocket() client: Socket, message: string) {
+    
+  }
+
+  @SubscribeMessage('accept')
+  acceptData(@ConnectedSocket() client: Socket, message: string) {
+    
+  }
+
+  @SubscribeMessage('match_made')
+  matchMadeData(@ConnectedSocket() client: Socket, message: string) {
+    
+  }
+  
+  @SubscribeMessage('game_start')
+  gameStartData(@ConnectedSocket() client: Socket, message: string) {
+    
   }
 }
