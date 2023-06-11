5e948f86ca71cd4279b749bc396986fd4f3f8f96
Recordum <mingyu.oh15b@gmail.com>
2023-06-12 06:56:59 +0900

    Feat: [#20] GamService: loading 완료시 GameReday 메시지 BroadCast

--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -1,7 +1,8 @@
 import { GameRoomHandler } from './room/game.room.handler';
-import { Inject, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { Socket } from 'socket.io';
 import { GameRoom } from './room/game.room';
+import { UserGameDto } from 'src/user/dto/user.game.dto';
 
 @Injectable()
 export class GameService {
@@ -10,5 +11,23 @@ export class GameService {
   public loadData(user: Socket) {
     const gameRoom: GameRoom = this.gameRoomHandler.findRoomBySocket(user);
     user.emit('loading', gameRoom.getGameSongDto);
+  }
+
+  public gameReady(user: Socket) {
+    const gameRoom: GameRoom = this.gameRoomHandler.findRoomBySocket(user);
+
+    this.gameRoomHandler.increaseAcceptCount(user);
+    if (this.gameRoomHandler.isGameRoomReady(gameRoom)) {
+      gameRoom.resetAcceptCount();
+      this.broadCastGameRoom(gameRoom);
+    }
+  }
+
+  public broadCastGameRoom(gameRoom: GameRoom) {
+    const userList: Array<UserGameDto> =
+      this.gameRoomHandler.findUsersInRoom(gameRoom);
+    for (const user of userList) {
+      user.getSocket().emit('game_ready', true);
+    }
   }
 }
