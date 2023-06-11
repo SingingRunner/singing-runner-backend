47648edba00db4751d63bcb39a72b34fe68788dc
Recordum <mingyu.oh15b@gmail.com>
2023-06-11 10:05:52 +0900

    Feat: [#2] EventsGateway: matchMakingData() 구현

--- src/game/game.gateway.ts
+++ src/game/game.gateway.ts
@@ -1,44 +1,46 @@
-import { ConnectedSocket, OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { UserGameDto } from './../user/dto/user.game.dto';
+import {
+  ConnectedSocket,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
+import { GameService } from './game.service';
+import { find } from 'rxjs';
 
 @WebSocketGateway()
-export class EventsGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect{
-  
+export class EventsGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
   @WebSocketServer() server: Server;
-  
-  constructor(
-    
-  ) {}
+
+  constructor(private gameService: GameService) {}
 
   afterInit(server: Server) {
     console.log('Socket.io server initialized in ');
   }
 
-  handleConnection(@ConnectedSocket() client: Socket) {
-    console.log(`Client connected: ${client.id}`);
+  handleConnection(@ConnectedSocket() user: Socket) {
+    console.log(`Client connected: ${user.id}`);
   }
 
-  handleDisconnect(@ConnectedSocket() client: Socket) {
-    console.log(`Client disconnected: ${client.id}`);
+  handleDisconnect(@ConnectedSocket() user: Socket) {
+    console.log(`Client disconnected: ${user.id}`);
   }
 
-  @SubscribeMessage('match_connected')
-  matchConnectedData(@ConnectedSocket() client: Socket, message: string) {
-    
-  }
+  @SubscribeMessage('match_making')
+  matchMakingData(@ConnectedSocket() user: Socket, userGameDto: UserGameDto) {
+    if (this.gameService.isMatchingAvailable(userGameDto)) {
+      const userList: Array<UserGameDto> =
+        this.gameService.findUsersInSameRoom(user);
 
-  @SubscribeMessage('accept')
-  acceptData(@ConnectedSocket() client: Socket, message: string) {
-    
-  }
-
-  @SubscribeMessage('match_made')
-  matchMadeData(@ConnectedSocket() client: Socket, message: string) {
-    
-  }
-  
-  @SubscribeMessage('game_start')
-  gameStartData(@ConnectedSocket() client: Socket, message: string) {
-    
+      for (const user of userList) {
+        user.socket.emit('match_making', true);
+      }
+    }
   }
 }
