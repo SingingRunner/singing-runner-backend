50e7a782bde7c4e398cf093f1d98b27e61b21471
Recordum <mingyu.oh15b@gmail.com>
2023-06-11 09:41:57 +0900

    Feat: [#2] GameService: findUsersinSameRoom 구현

--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -2,6 +2,8 @@ import { UserGameDto } from 'src/user/dto/user.game.dto';
 import { MatchMakingPolicy } from './match/match.making.policy';
 import { GameRoomHandler } from './room/game.room.handler';
 import { Inject, Injectable } from '@nestjs/common';
+import { Socket } from 'socket.io';
+import { GameRoom } from './room/game.room';
 
 @Injectable()
 export class GameService {
@@ -13,9 +15,23 @@ export class GameService {
 
   public isMatchingAvailable(userGameDto: UserGameDto): boolean {
     if (this.matchMakingPolicy.isQueueReady()) {
+      const userList: Array<UserGameDto> =
+        this.matchMakingPolicy.getAvailableUsers();
+
+      userList.push(userGameDto);
+      this.joinRoom(userList);
       return true;
     }
     this.matchMakingPolicy.joinQueue(userGameDto);
     return false;
+  }
+
+  private joinRoom(userList: Array<UserGameDto>) {
+    const gameRoom: GameRoom = this.gameRoomHandler.createRoom();
+    this.gameRoomHandler.addUser(gameRoom, userList);
+  }
+
+  public findUsersInSameRoom(user: Socket): Array<UserGameDto> {
+    return this.gameRoomHandler.findUsersInRoom(user);
   }
 }
