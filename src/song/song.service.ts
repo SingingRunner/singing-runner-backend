df81292e270eaa9175a00d2931b6442b1f423c36
camel0000 <camel0000@DESKTOP-1RUBIDF.localdomain>
2023-06-13 00:47:40 +0900

    Comment: [#3] SongService 클래스 주석 작성

--- src/song/song.service.ts
+++ src/song/song.service.ts
@@ -10,9 +10,13 @@ export class SongService {
     private readonly songRepository: Repository<Song>,
   ) {}
 
+  /* DB에서 모든 Song 데이터 가져와서 임의의 노래 반환하는 비동기 함수 */
   async getRandomSong(): Promise<Song> {
+    // DB에서 모든 Song 데이터 가져오기
     const songs = await this.songRepository.find();
+    // songs 배열의 길이만큼 랜덤한 인덱스 생성
     const randomIndex = Math.floor(Math.random() * songs.length);
+    // songs 배열에서 랜덤한 인덱스의 Song 반환
     return songs[randomIndex];
   }
 }
