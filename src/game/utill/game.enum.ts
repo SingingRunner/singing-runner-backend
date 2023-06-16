82e76ac0addb857c4f82d2e65a6c01fd9a496de7
Recordum <mingyu.oh15b@gmail.com>
2023-06-17 02:16:14 +0900

    Feat: [#50]userMatchTier enum 추가

--- src/game/utill/game.enum.ts
+++ src/game/utill/game.enum.ts
@@ -2,3 +2,11 @@ export enum GameRoomStatus {
   IN_GAME = 'inGame',
   MATCHING = 'matching',
 }
+
+export enum UserMatchTier {
+  BRONZE = 100,
+  SILVER = 200,
+  GOLD = 300,
+  PLATINUM = 400,
+  DIA = 500,
+}
