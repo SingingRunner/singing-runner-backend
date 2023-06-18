09d45cf9c982af7dffbdb8619be4fa1f49f2681c
camel0000 <camel0000@DESKTOP-1RUBIDF.localdomain>
2023-06-18 09:06:05 +0900

    Feat: [#54] User Entity 구현

---
+++ src/user/entity/user.entity.ts
@@ -0,0 +1,28 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('user')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  userId: string;
+
+  @Column({ type: 'varchar', length: 30 })
+  userEmail: string;
+
+  @Column({ type: 'varchar', length: 16 })
+  password: string;
+
+  @Column({ type: 'varchar', length: 24 })
+  nickName: string;
+
+  @Column({ type: 'tinyint', default: 0 })
+  userActive: boolean;
+
+  @Column({ type: 'tinyint', default: 0 })
+  userKeynote: boolean;
+
+  @Column({ type: 'smallint', default: 0 })
+  userMmr: number;
+
+  @Column({ type: 'mediumint', default: 0 })
+  userPoint: number;
+}
