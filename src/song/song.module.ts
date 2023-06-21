5e7a848825fbccee363d16858e6ff5bdce95ac6a
Recordum <mingyu.oh15b@gmail.com>
2023-06-21 13:39:32 +0900

    Feat: [#98] SongModule: SongResolver 등록

--- src/song/song.module.ts
+++ src/song/song.module.ts
@@ -2,11 +2,12 @@ import { Module } from "@nestjs/common";
 import { SongService } from "./song.service";
 import { Song } from "./entities/song.entity";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { SongResolver } from "./song.resolver";
 
 @Module({
   imports: [TypeOrmModule.forFeature([Song])],
   controllers: [],
-  providers: [SongService],
+  providers: [SongService, SongResolver],
   exports: [SongService],
 })
 export class SongModule {}
