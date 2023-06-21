6d35e6bec3c14aff9865f83dea5b8c276a372695
Recordum <mingyu.oh15b@gmail.com>
2023-06-22 06:50:49 +0900

    Modify: [#151] Social entitiy 주키 설정

--- src/social/entity/social.entity.ts
+++ src/social/entity/social.entity.ts
@@ -1,8 +1,14 @@
 import { User } from "src/user/entity/user.entity";
-import { Column, Entity, ManyToOne, JoinColumn } from "typeorm";
+import { Column, Entity, ManyToOne, JoinColumn, PrimaryColumn } from "typeorm";
 
 @Entity("social")
 export class Social {
+  @PrimaryColumn()
+  userId: number;
+
+  @PrimaryColumn()
+  friendId: number;
+
   @ManyToOne(() => User)
   @JoinColumn({ name: "userId" })
   user: User;
