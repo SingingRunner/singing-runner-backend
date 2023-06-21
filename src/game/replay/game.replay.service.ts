9bd2f3f5a9db58ca071a734ef0ae7aaf4d7c7737
Park HyeongIn <hyeongin2024@gmail.com>
2023-06-21 19:28:14 +0900

    Feat: [#95] socket event 추가

--- src/game/replay/game.replay.service.ts
+++ src/game/replay/game.replay.service.ts
@@ -81,7 +81,7 @@ export class GameReplayService {
           userId: gameReplay.player2Id,
           character: gameReplay.player2Character,
         });
-        user.emit("replay", {
+        user.emit("load_replay", {
           gameSong: gameSong,
           characterList: characterList,
         });
@@ -99,6 +99,7 @@ export class GameReplayService {
       });
     // aws s3 에서 gameEvent 를 가져와서 setTimeout으로 socket emit event 예약
     // fetch 과정이 조금 걸릴 수도 있으니 서버에서 파일 로드가 완료될 때까지 클라이언트 대기하게끔 소켓 이벤트 추가해야할듯
+    user.emit("start_replay");
     console.log(gameEvent);
   }
 }
