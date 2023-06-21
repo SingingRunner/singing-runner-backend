d79e548901d8b5e68aa843a77b53faae448c004a
Recordum <mingyu.oh15b@gmail.com>
2023-06-21 19:08:30 +0900

    Feat: [#115] Invite: 클래스 추가

---
+++ src/game/custom-mode/invite.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@nestjs/common";
+import { HostUserDto } from "src/auth/user/dto/host-user.dto";
+
+@Injectable()
+export class Invite {
+  private inviteMap: Map<string, HostUserDto[]> = new Map();
+
+  public inviteFriend(friendId: string, host: HostUserDto) {
+    let inviteQueue = this.inviteMap.get(friendId);
+    if (inviteQueue === null || inviteQueue === undefined) {
+      inviteQueue = [];
+      this.inviteMap.set(friendId, inviteQueue);
+    }
+    inviteQueue.push(host);
+  }
+
+  public deleteInvite(userId: string) {
+    this.inviteMap.delete(userId);
+  }
+
+  public cancleInvite(userId: string, host: HostUserDto) {
+    const inviteQueue = this.inviteMap.get(userId);
+    if (inviteQueue === null || inviteQueue === undefined) {
+      throw new Error("userID is undefined or null");
+    }
+    inviteQueue.filter((hostInfo) => hostInfo.getUserId() !== host.getUserId());
+  }
+}
