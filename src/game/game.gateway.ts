682259fd3f3bcd8b43d525188d2a860098c2f892
camel0000 <camel0000@naver.com>
2023-06-24 19:38:37 +0900

    Fix: [#211] gameTerminated 메소드명 수정

--- src/game/game.gateway.ts
+++ src/game/game.gateway.ts
@@ -169,7 +169,7 @@ export class GameGateway
   }
 
   @SubscribeMessage("game_terminated")
-  async gameTermintated(
+  async gameTerminated(
     @ConnectedSocket() user: Socket,
     @MessageBody() userScoreDto: UserScoreDto
   ) {
