01928c998951a527b0d69de9251077c51891fa82
camel0000 <camel0000@naver.com>
2023-06-24 20:10:09 +0900

    Fix: [#213] gameTerminatedList 수정: UserScoreDto nickname 반영

--- src/game/rank/dto/user-score.dto.ts
+++ src/game/rank/dto/user-score.dto.ts
@@ -1,14 +1,20 @@
 export class UserScoreDto {
   private userId: string;
+  private nickname: string;
   private score: number;
 
-  constructor($userId: string, $score: number) {
+  constructor($userId: string, $nickname: string, $score: number) {
     this.userId = $userId;
+    this.nickname = $nickname;
     this.score = $score;
   }
 
   public getUserId(): string {
     return this.userId;
+  }
+
+  public getNickname(): string {
+    return this.nickname;
   }
 
   public getScore(): number {
