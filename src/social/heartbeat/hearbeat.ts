d7ab751b570cf3d45484193ac9f8a829a7d30a6c
Recordum <mingyu.oh15b@gmail.com>
2023-06-25 18:52:22 +0900

    Feat: [#269] HeartBeatimpl 구현

--- src/social/heartbeat/hearbeat.ts
+++ src/social/heartbeat/hearbeat.ts
@@ -1,6 +1,5 @@
 export interface HeartBeat {
-  setHeartBeatMap(userId: string, logingAt: Date): void;
+  updateHeartBeatMap(userId: string, updateAt: Date): void;
   deleteHeartBeatMap(userId: string): void;
-  updateHeartBeatMap(userId: string, activeAt: Date): void;
   updateDB(userId: string): void;
 }
