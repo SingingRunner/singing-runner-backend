b9a8777eafed0cd0174e970093b9b2ec945ab892
Park HyeongIn <hyeongin2024@gmail.com>
2023-07-01 02:38:47 +0900

    Feat: [#361] song service에 redis 추가

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from "./app.module";
 import { IoAdapter } from "@nestjs/platform-socket.io";
 import cookieParser from "cookie-parser";
 import { urlencoded, json } from "body-parser";
+import { ErrorInterceptor } from "./ErrorInterceptor";
 
 class MyIoAdapter extends IoAdapter {
   createIOServer(port: number, options?: any): any {
@@ -19,6 +20,7 @@ class MyIoAdapter extends IoAdapter {
 }
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new ErrorInterceptor());
   app.use(cookieParser());
   app.use(json({ limit: "50mb" }));
   app.use(urlencoded({ limit: "50mb", extended: true }));
--- src/song/song.service.ts
+++ src/song/song.service.ts
@@ -3,33 +3,42 @@ import { Like, Repository } from "typeorm";
 import { Song } from "./entities/song.entity";
 import { InjectRepository } from "@nestjs/typeorm";
 import { GameSongDto } from "./dto/game-song.dto";
+import { InjectRedis } from "@liaoliaots/nestjs-redis";
+import { Redis } from "@nestjs-modules/ioredis";
 
 @Injectable()
 export class SongService {
   constructor(
     @InjectRepository(Song)
-    private readonly songRepository: Repository<Song>
-  ) {}
+    private readonly songRepository: Repository<Song>,
+    @InjectRedis() private readonly redis: Redis
+  ) {
+    this.songRepository.find().then((songs: Song[]) => {
+      this.redis.set("songList", JSON.stringify(songs), "EX", 60 * 60 * 24);
+      songs.forEach((song: Song) => {
+        this.redis.set(
+          "song:${song.songId}.${song.songTitle}",
+          JSON.stringify(song),
+          "EX",
+          60 * 60 * 24
+        );
+      });
+    });
+  }
 
   /* DB에서 모든 Song 데이터 가져와서 임의의 노래 반환하는 비동기 함수 */
   public async getRandomSong(): Promise<GameSongDto> {
     // DB에서 모든 Song 데이터 가져오기
-    const songs: Song[] = await this.songRepository.find();
+    let songs: Song[] = JSON.parse((await this.redis.get("songList")) || "");
+    if (songs === null || songs === undefined || songs.length === 0) {
+      songs = await this.songRepository.find();
+    }
     // songs 배열의 길이만큼 랜덤한 인덱스 생성
     const randomIndex: number = Math.floor(Math.random() * songs.length);
     const song: Song = songs[randomIndex];
 
     // Song Entity를 GameSongDto로 변환
     return new GameSongDto(song);
-  }
-
-  public async getAllSong(): Promise<GameSongDto[]> {
-    const songs: Song[] = await this.songRepository.find();
-    const gameSongDtoList: GameSongDto[] = [];
-    for (const song of songs) {
-      gameSongDtoList.push(new GameSongDto(song));
-    }
-    return gameSongDtoList;
   }
 
   public async searchSong(
