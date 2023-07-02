02a812150aefc6a95a820f063ed973ebaf67bd5d
Recordum <mingyu.oh15b@gmail.com>
2023-07-02 13:33:12 +0900

    Fix: [#396] DB 수정

--- src/social/social.controller.ts
+++ src/social/social.controller.ts
@@ -8,7 +8,6 @@ export class SocialController {
 
   @Sse("invite/:userId")
   invite(@Param("userId") userId: string): Observable<any> {
-    console.log("SSE invite SUBSCRIPTION", userId);
     return this.socialService.inviteEvents(userId).pipe(
       map((event) => ({
         data: { host: event.host },
@@ -18,7 +17,6 @@ export class SocialController {
 
   @Sse("notification/:userId")
   notifictaion(@Param("userId") userId: string): Observable<any> {
-    console.log("SSE Noti SUBSCRIPTION", userId);
     return this.socialService.notificationEvensts(userId).pipe(
       map((event) => ({
         data: { alarm: event.alarm },
