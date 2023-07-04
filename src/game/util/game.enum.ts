78e0045ee9da2b8ce667fee670bf73e5961e5a25
Recordum <mingyu.oh15b@gmail.com>
2023-07-04 16:45:20 +0900

    Refactor : [#442] 소켓 메시지 enum 타입 추가

--- src/game/util/game.enum.ts
+++ src/game/util/game.enum.ts
@@ -10,3 +10,23 @@ export enum UserMatchTier {
   PLATINUM = 4000,
   DIAMOND = 5000,
 }
+
+export enum Message {
+  MATCH_MAKING = "match_making",
+  ACCEPT = "accept",
+  LOADING = "loading",
+  GAME_READY = "game_ready",
+  USE_ITEM = "use_item",
+  GET_ITEM = "get_item",
+  GAME_MODE = "game_mode",
+  ESCAPE_ITEM = "escape_item",
+  SCORE = "score",
+  GAME_TERMINATED = "game_terminated",
+  INVITE = "invite",
+  CREATE_CUSTOM = "create_custom",
+  SET_SONG = "set_song",
+  LEAVE_ROOM = "leave_room",
+  CUSTOM_START = "custom_start",
+  LOAD_REPLAY = "load_replay",
+  START_REPLAY = "start_replay",
+}
